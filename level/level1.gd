8cbd6c3268afaa868bb8657bbdb9a8ce62f53130
Samuel Sarette <samuelsarette@linux.com>
2024-01-27 13:04:49 -0500

    made level and theming progress

--- level/level1.gd
+++ level/level1.gd
@@ -1,29 +1,66 @@
 extends Node3D
 
+var current_section := 0
+
 var section_targets := [
 	1,
 	1,
+	0,
 	3
 ]
 
-@onready
-var section_1 : PhantomCamera3D = $Shots/Section1
+var level_is_complete := false
+signal level_complete
 
 @onready
-var section_2 : PhantomCamera3D = $Shots/Section2
+var section_cams : Array[PhantomCamera3D] = [
+	$Shots/Section1,
+	$Shots/Section2,
+	$Shots/Section3,
+	$Shots/Section4
+]
+
+@onready
+var sfx : AudioStreamPlayer = $"Player/Sfx"
+
+@onready
+var music : AudioStreamPlayer = $"Music"
+
+@onready
+var voice : AudioStreamPlayer = $"Player/Voice"
+
+const READY : AudioStreamOggVorbis = preload("res://addons/kenney voiceover pack fighter/ready.ogg")
+const GO : AudioStreamOggVorbis = preload("res://addons/kenney voiceover pack/voice type 2/go.ogg")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	voice.stream = READY
+	voice.play(0)
+	await voice.finished
+	voice.stream = GO
+	voice.play(0)
 
 
 func _on_target_shot():
-	section_targets[1] -= 1
-	if section_targets[1] <= 0:
-		section_1.set_priority(0)
-		section_2.set_priority(1)
+	if level_is_complete:
+		return
+	section_targets[current_section] -= 1
+	update_camera()
+
+
+func update_camera() -> void:
+	if level_is_complete:
+		return
+
+	if section_targets[current_section] > 0:
+		return
+	section_cams[current_section].set_priority(0)
+	current_section += 1
+	if current_section >= section_cams.size():
+		level_is_complete = true
+		level_complete.emit()
+	else:
+		section_cams[current_section].set_priority(1)
+		if section_targets[current_section] <= 0:
+			await get_tree().create_timer(1).timeout
+			update_camera()
